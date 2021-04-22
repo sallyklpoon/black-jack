ed316469dcd288d39fe7f58dacf7744c1e1e88da
Sally Poon <sallypoon@hotmail.com>
2021-04-22 16:03:33 -0700

    ADD Card constant headers

---
+++ blackjack.py
@@ -0,0 +1,21 @@
+"""
+Name: Sally Poon
+Student Number: A01232177
+
+This is a module to play BlackJack 21.
+"""
+
+
+def CARD_VALUES():
+    """Return all the possible card values in a deck excluding joker.
+
+    :return: a tuple
+    """
+    pass
+
+
+def CARD_SUITS():
+    """Return all the possible card suits in a deck.
+
+    :return: a tuple
+    """
