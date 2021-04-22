dc3aec90a984fe2c86049c9261242fe8acb5f3a0
Sally Poon <sallypoon@hotmail.com>
2021-04-22 16:52:21 -0700

    ADD Bank and Player constants/class details

--- blackjack.py
+++ blackjack.py
@@ -9,7 +9,9 @@ This is a module to play BlackJack 21.
 def CARD_VALUES():
     """Return all the possible card values in a deck excluding joker.
 
-    :return: a tuple
+    The key is the face of a card and the value is the actual value of the card in BlackJack
+
+    :return: a dictionary
     """
     pass
 
@@ -19,3 +21,45 @@ def CARD_SUITS():
 
     :return: a tuple
     """
+    pass
+
+
+def MINIMUM_BET():
+    """Return the minimum bet amount.
+
+    :return: an integer, the minimum bet amount
+    """
+    pass
+
+
+def START_BANK():
+    """Return the initial amount for the player's betting balance.
+
+    :return: an integer, the starting amount in player's balance
+    """
+    pass
+
+
+class Player:
+
+    def __init__(self):
+        """Instantiate an instance of Player.
+
+        :return: an instance of the Player class
+        """
+        pass
+
+    def __str__(self):
+        """Return the string when Player is printed.
+
+        :return: a string, when player is printed as a string
+        """
+        pass
+
+    def __repr__(self):
+        """Return the representational string when Player is called as an object.
+
+        :return: a string, when the player is called as an object
+        """
+        pass
+
